10b8955a6b2e849e52490b9d78daa1361b613f3d
mmoha.Muna <mmoha.munamohamed2890@gmail.com>
2025-06-11 14:41:48 -0400

    added notes

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require("express");
 
-
 const app = express();
-
-
 const port = 3000;
 
 
 
 
-
+app.use(express.json())
 
 app.get("/", (req, res) => {
   res.send("Server is running");
