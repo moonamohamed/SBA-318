07557e1af3049a18263f045c811b2f2c79529963
mmoha.Muna <mmoha.munamohamed2890@gmail.com>
2025-06-15 00:30:48 -0400

    added a file

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require("express");
-
 const app = express();
 const port = 3000;
 
 
 
 
-app.use(express.json())
-
-app.get("/", (req, res) => {
-  res.send("Server is running");
+app.get("/:reminder", (req, res) => {
+  res.send("Wake Up");
 });
 app.listen(port, () => {
   console.log("Server is running")
